3f3a2162f8cbe0781c297838d7e8f89d07dbd402
Miłosz Markowski <markowskimilosz989@gmail.com>
2025-04-17 20:37:44 +0200

    fihf

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,8 +1,15 @@
 output "sql_connection_string" {
   description = "Sensitive ADO.NET connection string to the Azure SQL Database with SQL authentication"
-  value       = "Server=${azurerm_mssql_server.sql.fully_qualified_domain_name};Database=${azurerm_mssql_database.db.name};User ID=${var.sql_admin_name};Password=${random_password.sql_password.result};Encrypt=true;Connection Timeout=30;"
   sensitive   = true
+  value       = format(
+    "Server=tcp:%s,1433;Initial Catalog=%s;Persist Security Info=False;User ID=%s;Password=%s;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+    azurerm_mssql_server.sql.fully_qualified_domain_name,
+    azurerm_mssql_database.db.name,
+    var.sql_admin_name,
+    random_password.sql_password.result
+  )
 }
+
 
 output "sql_server_fqdn" {
   description = "Fully qualified domain name of the Azure SQL Server (FQDN)"
--- task06/outputs.tf
+++ task06/outputs.tf
@@ -7,9 +7,3 @@ output "app_hostname" {
   description = "Hostname of the deployed Azure Linux Web App"
   value       = module.webapp.app_hostname
 }
-
-output "sql_connection_string" {
-  description = "Sensitive SQL connection string from SQL module"
-  value       = module.sql.sql_connection_string
-  sensitive   = true
-}
